fbc81e27fb9b990dd33e7ab3b6f47502e60cbff7
Anthony Marquez <anthonym.kentico@gmail.com>
2022-04-19 10:24:13 -0400

    Add files via upload

---
+++ SSLRequestModule/SSLRequestModule.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Web;
+using System.Collections.Specialized;
+
+using CMS;
+using CMS.DataEngine;
+using CMS.Base;
+using CMS.Helpers;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using System.Collections.Generic;
+
+
+// Registers the custom module into the system
+[assembly: RegisterModule(typeof(SSLRequestModule))]
+
+public class SSLRequestModule : Module
+{
+    // Module class constructor, the system registers the module under the name "SSLRequests"
+    public SSLRequestModule()
+        : base("SSLRequests")
+    {
+    }
+
+    // Contains initialization code that is executed when the application starts
+    protected override void OnInit()
+    {
+        base.OnInit();
+
+        // Assigns a handler called before each request is processed
+        RequestEvents.Prepare.Execute += HandleSSLRequests;
+
+        // Sets the URL port used for HTTPS requests
+        URLHelper.SSLUrlPort = 443;
+    }
+
+    // Checks if requests are forwarded as SSL
+    private static void HandleSSLRequests(object sender, EventArgs e)
+    {
+#if NETFRAMEWORK
+        if ((HttpContext.Current != null) && (HttpContext.Current.Request != null))
+        {
+            // Loads the request headers as a collection
+            NameValueCollection headers = HttpContext.Current.Request.Headers;
+
+            // Gets the value from the X-Forwarded-Ssl header
+            string forwardedSSL = headers.Get("X-Forwarded-Ssl");
+            string apacheForwardedSSL = headers.Get("X-Forwarded-Proto");
+            RequestContext.IsSSL = false;
+
+            // Checks if the original request used HTTPS
+            if (String.Equals(forwardedSSL, "on", StringComparison.OrdinalIgnoreCase) || String.Equals(apacheForwardedSSL, "https", StringComparison.OrdinalIgnoreCase))
+            {
+                RequestContext.IsSSL = true;
+            }
+        }
+#endif
+    }
+}
---
+++ SSLRequestModule/SSLRequestModuleMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using CMS.Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+public class SSLRequestModuleMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public SSLRequestModuleMiddleware(RequestDelegate next)
+    {
+        _next = next;
+        URLHelper.SSLUrlPort = 443;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        // Do something with context near the beginning of request processing.
+        if (context.Request != null)
+        {
+            context.Request.Headers.TryGetValue("X-Forwarded-Proto", out var apacheHeader);
+            context.Request.Headers.TryGetValue("X-Forwarded-Ssl", out var normalHeader);
+            RequestContext.IsSSL = false;
+            // Checks if the original request used HTTPS
+            if (String.Equals(normalHeader, "on", StringComparison.OrdinalIgnoreCase) || String.Equals(apacheHeader, "https", StringComparison.OrdinalIgnoreCase))
+            {
+                RequestContext.IsSSL = true;
+            }
+        }
+        await _next.Invoke(context);
+
+        // Clean up.
+    }
+}
+
+public static class SSLRequestModuleMiddlewareExtensions
+{
+    public static IApplicationBuilder UseSSLRequestModuleMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<SSLRequestModuleMiddleware>();
+    }
+}
+
+
+// https://docs.microsoft.com/en-us/aspnet/core/migration/http-modules?view=aspnetcore-6.0
